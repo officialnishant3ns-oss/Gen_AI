1cc9d682d440e2716fe79544993b10189527e1ba
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-03-01 10:24:27 +0530

    added user models

---
+++ src/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const userschema = new mongoose.Schema({
+     username:{
+        type:String,
+        unique:[true, 'Username already taken'],
+        require:true
+     },
+      email:{
+        type:String,
+        unique:[true, 'Account Already Exist'],
+        require:true
+     },
+      password:{
+        type:String,
+        require:true
+     },
+
+})
+
+const User  = mongoose.model('User',userschema)
+
+export default User
