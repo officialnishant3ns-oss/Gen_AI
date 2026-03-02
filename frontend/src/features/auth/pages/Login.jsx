d12ed5c30d1fc3755537eac1a0b286373bbbcbdd
NISHANT SINGH <official.nishant.3ns@gmail.com>
2026-03-02 19:10:31 +0530

    added login and register there

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
+    const [fullname, setFullname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+            const res = await api.post('/user/login', {
+                email,
+                password
+            })
+
+            console.log('Register success:', res.data)
+
+            localStorage.setItem('token', res.data.token)
+            localStorage.setItem('user', JSON.stringify(res.data.user))
+
+            setFullname('')
+            setEmail('')
+            setPassword('')
+        } catch (err) {
+            console.error('Login failed:', err.response?.data || err.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 via-gray-950 to-black">
+            <form
+                onSubmit={submitHandler}
+                className="flex flex-col gap-5 bg-gray-900/70 backdrop-blur-xl border border-gray-800 p-10 rounded-3xl shadow-2xl w-full max-w-md h-100"
+            >
+
+                <h2 className="text-2xl font-bold text-center text-white tracking-wide mb-6">
+                    Login to your account
+                </h2>
+                <input
+                autoComplete="off"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    placeholder="Email Address"
+                    className="bg-gray-800 text-white border border-gray-700 p-4 rounded-xl outline-none focus:ring-2 focus:ring-indigo-500 transition"
+                />
+                <input
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    placeholder="Password"
+                    className="bg-gray-800 text-white border border-gray-700 p-4 rounded-xl outline-none focus:ring-2 focus:ring-indigo-500 transition"
+                />
+
+                <button
+                    disabled={loading}
+                    className="bg-indigo-600 hover:bg-indigo-700 active:scale-[0.98] disabled:opacity-50 text-white font-semibold py-3 rounded-xl mt-3 transition-all duration-200 shadow-lg shadow-indigo-900/40"
+                >
+                    {loading ? 'Loging Account...' : 'Login'}
+                </button>
+
+            </form>
+        </div>
+    )
 }
 
 export default Login
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Register = () => {
+  const [fullname, setFullname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    try {
+      const res = await api.post('/user/register', {
+        username: fullname,
+        email,
+        password
+      })
+
+      console.log('Login success:', res.data)
+
+      localStorage.setItem('token', res.data.token)
+      localStorage.setItem('user', JSON.stringify(res.data.user))
+
+      setFullname('')
+      setEmail('')
+      setPassword('')
+    } catch (err) {
+      console.error('Login failed:', err.response?.data || err.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <div>
-      Register
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 via-gray-950 to-black">
+      <form
+        onSubmit={submitHandler}
+        className="flex flex-col gap-5 bg-gray-900/70 backdrop-blur-xl border border-gray-800 p-10 rounded-3xl shadow-2xl w-full max-w-md"
+      >
+
+        <h2 className="text-2xl font-bold text-center text-white tracking-wide">
+          Create Account
+        </h2>
+
+        <input
+          value={fullname}
+          onChange={(e) => setFullname(e.target.value)}
+          type="text"
+          placeholder="Full Name"
+          className="bg-gray-800 text-white border border-gray-700 p-4 rounded-xl outline-none focus:ring-2 focus:ring-indigo-500 transition"
+        />
+
+        <input
+       
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email Address"
+       autoComplete="new-email"
+          className="bg-gray-800 text-white border border-gray-700 p-4 rounded-xl outline-none focus:ring-2 focus:ring-indigo-500 transition"
+        />
+
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          className="bg-gray-800 text-white border border-gray-700 p-4 rounded-xl outline-none focus:ring-2 focus:ring-indigo-500 transition"
+        />
+
+        <button
+          disabled={loading}
+          className="bg-indigo-600 hover:bg-indigo-700 active:scale-[0.98] disabled:opacity-50 text-white font-semibold py-3 rounded-xl mt-2 transition-all duration-200 shadow-lg shadow-indigo-900/40"
+        >
+          {loading ? 'Creating Account...' : 'Register'}
+        </button>
+
+      </form>
     </div>
   )
 }
